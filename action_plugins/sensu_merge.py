64a65dabe60ba1671ab4438aff0244b6a7288acf
Josh Benner <josh@bennerweb.com>
2017-09-29 21:00:31 -0400

    Plugin install, tests

--- action_plugins/sensu_merge.py
+++ action_plugins/sensu_merge.py
@@ -7,10 +7,12 @@ class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
         suffix = self._task.args.get('suffix')
         varname = self._task.args.get('var_name')
+        mergetype = self._task.args.get('type', 'dict')
 
-        merged = {}
+        merged = {} if mergetype == 'dict' else []
+        mergefunc = merged.update if mergetype == 'dict' else merged.extend
         for var in (v for v in task_vars.keys() if v.endswith(suffix)):
-            merged.update(task_vars[var])
+            mergefunc(task_vars[var])
 
         return dict(
             ansible_facts={varname: self._templar.template(merged)},
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,47 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_packages(host):
+    assert host.package('sensu').is_installed
 
+
+def test_main_config(host):
+    f =  host.file('/etc/sensu/config.json')
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.is_file
+    assert f.user == 'sensu'
+    assert f.group == 'sensu'
+    assert f.mode == 0o600
+    assert f.contains('rabbitmq')
+
+
+def test_check_config(host):
+    f = host.file('/etc/sensu/conf.d/checks.json')
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'sensu'
+    assert f.group == 'sensu'
+    assert f.mode == 0o600
+    assert f.contains('check-cpu.rb')
+
+
+def test_server_running(host):
+    server = host.service('sensu-server')
+    assert server.is_running
+    assert server.is_enabled
+
+
+def test_api_running(host):
+    api = host.service('sensu-api')
+    assert api.is_running
+    assert api.is_enabled
+
+
+def test_client_running(host):
+    client = host.service('sensu-client')
+    assert client.is_running
+    assert client.is_enabled
+
+
+def test_api_listening(host):
+    assert host.socket('tcp://0.0.0.0:4567').is_listening
